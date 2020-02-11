3420efbe6d94d6faed15d28895618e1dd801021f
triard <triard78@gmail.com>
2020-02-11 19:56:45 +0700

    finally :)

--- app/src/main/java/id/putraprima/marketplacelayout/MainActivity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/MainActivity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
     }
 }
